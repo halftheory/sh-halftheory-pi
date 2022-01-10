f4a9518134f2f4ff78d390b2cf74945b7119541e
halftheory <mail@halftheory.com>
2022-01-10 23:24:57 +0100

    update

--- halftheory_play.sh
+++ halftheory_play.sh
@@ -6,13 +6,18 @@
 # chmod +x /home/pi/halftheory_play.sh
 # sudo ln -s /home/pi/halftheory_play.sh /bin/play
 
+# usage
+if [ -z $1 ]; then
+    BASENAME=`basename $0`
+    echo "> Usage: $BASENAME [file]"
+    exit 1
+fi
+
 if [ $1 ]; then
 	CMD_OMXPLAYER=`omxplayer -b -o local --no-osd --timeout 5 $1 | grep -oP "unknown|unable|omx_err|ERROR|unrecognized"`
 	if [ ! "$CMD_OMXPLAYER" = "" ]; then
 		ffplay -hide_banner -v quiet -fs -fast -framedrop -infbuf -autoexit -exitonkeydown -fflags discardcorrupt $1
 	fi
-else
-	echo "> Usage: play [file]"
 fi
 
 exit 0
