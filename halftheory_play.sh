093b8f8ae0c0ef747c0892334df8ef12c135e892
Half/theory <noreply@halftheory.com>
2022-01-21 15:33:22 +0100

    update

--- halftheory_play.sh
+++ halftheory_play.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
 # import vars
-DIRNAME=`dirname "$0"`
-CMD_TEST=`readlink "$0"`
+CMD_TEST="$(readlink "$0")"
 if [ ! "$CMD_TEST" = "" ]; then
-    DIRNAME=`dirname "$CMD_TEST"`
+	DIRNAME="$(dirname "$CMD_TEST")"
+else
+	DIRNAME="$(dirname "$0")"
 fi
 if [ -f "$DIRNAME/halftheory_vars.sh" ]; then
-    . $DIRNAME/halftheory_vars.sh
+	. $DIRNAME/halftheory_vars.sh
 else
 	echo "Error in $0 on line $LINENO. Exiting..."
-    exit 1
+	exit 1
 fi
 
 SCRIPT_ALIAS="play"
 
 # usage
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "> Usage: $SCRIPT_ALIAS [file]"
     exit 1
 # install
 elif [ "$1" = "-install" ]; then
-	FILE_SCRIPT=$0
-	CMD_TEST=`readlink "$0"`
-	if [ ! "$CMD_TEST" = "" ]; then
-	    FILE_SCRIPT=$CMD_TEST
+	if script_install "$0" "$DIR_SCRIPTS/$SCRIPT_ALIAS" "sudo"; then
+		echo "> Installed."
+		exit 0
+	else
+		echo "Error in $0 on line $LINENO. Exiting..."
+		exit 1
 	fi
-	chmod $CHMOD_FILES $FILE_SCRIPT
-	chmod +x $FILE_SCRIPT
-	${MAYBE_SUDO}rm $DIR_SCRIPTS/$SCRIPT_ALIAS > /dev/null 2>&1
-	${MAYBE_SUDO}ln -s $FILE_SCRIPT $DIR_SCRIPTS/$SCRIPT_ALIAS
-	echo "> Installed."
-	exit 0
 # uninstall
 elif [ "$1" = "-uninstall" ]; then
-	${MAYBE_SUDO}rm $DIR_SCRIPTS/$SCRIPT_ALIAS > /dev/null 2>&1
-	echo "> Uninstalled."
-	exit 0
+	if script_uninstall "$0" "$DIR_SCRIPTS/$SCRIPT_ALIAS" "sudo"; then
+		echo "> Uninstalled."
+		exit 0
+	else
+		echo "Error in $0 on line $LINENO. Exiting..."
+		exit 1
+	fi
 fi
 
-if [ ! -e "$1" ]; then
+if [ ! -e "$*" ]; then
 	echo "Error in $0 on line $LINENO. Exiting..."
     exit 1
 fi
@@ -47,21 +48,24 @@ fi
 BOOL_FALLBACK=true
 
 if is_which "cvlc" && is_opengl_legacy; then
-	VLC_VERBOSE=0 cvlc $1 vlc://quit --no-osd --fullscreen --align 0 --video-on-top --preferred-resolution -1 --play-and-exit
+	CMD_TEST="VLC_VERBOSE=0 cvlc $(quote_string_with_spaces "$*") vlc://quit --no-osd --fullscreen --align 0 --video-on-top --preferred-resolution -1 --play-and-exit"
+	eval "$CMD_TEST"
 	sleep 1
-	CMD_TEST=`pidof vlc`
-	if [ ! "$CMD_TEST" = "" ]; then
+	if is_process_running "vlc"; then
 		BOOL_FALLBACK=false
 	fi
 elif is_which "omxplayer"; then
-	CMD_TEST=`omxplayer -b -o local --no-osd --timeout 5 $1 | grep -oP "unknown|unable|ERROR|unrecognized|omx_err"`
-	if [ "$CMD_TEST" = "" ]; then
+	CMD_TEST="omxplayer -b -o local --no-osd --timeout 5 $(quote_string_with_spaces "$*")"
+	eval "$CMD_TEST"
+	sleep 1
+	if is_process_running "omxplayer.bin"; then
 		BOOL_FALLBACK=false
 	fi
 fi
 
 if [ $BOOL_FALLBACK = true ] && is_which "ffplay"; then
-	ffplay -hide_banner -v quiet -fs -fast -framedrop -infbuf -autoexit -exitonkeydown -fflags discardcorrupt $1
+	CMD_TEST="ffplay -hide_banner -v quiet -fs -fast -framedrop -infbuf -autoexit -exitonkeydown -fflags discardcorrupt $(quote_string_with_spaces "$*")"
+	eval "$CMD_TEST"
 fi
 
 exit 0
