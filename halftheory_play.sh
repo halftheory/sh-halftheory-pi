92184df11e52088ab21f067e21ad3f13d38e91d4
halftheory <mail@halftheory.com>
2023-06-04 01:06:31 +0200

    update

--- halftheory_play.sh
+++ halftheory_play.sh
@@ -18,22 +18,18 @@ SCRIPT_ALIAS="play"
 
 # usage
 if [ -z "$1" ] || [ "$1" = "-help" ]; then
-    echo "> Usage: $SCRIPT_ALIAS [file]"
-    exit 1
+	echo "> Usage: $SCRIPT_ALIAS [file]"
+	exit 1
 # install
 elif [ "$1" = "-install" ]; then
 	if script_install "$0" "$DIR_SCRIPTS/$SCRIPT_ALIAS" "sudo"; then
 		# depends
 		if has_arg "$*" "-depends" && [ ! "$(get_system)" = "Darwin" ]; then
 			BOOL_FALLBACK=false
-			if is_opengl_legacy; then
-				if ! maybe_apt_install "cvlc" "vlc"; then
-					BOOL_FALLBACK=true
-				fi
-			else
-				if ! maybe_apt_install "omxplayer"; then
-					BOOL_FALLBACK=true
-				fi
+			if ! maybe_apt_install "cvlc" "vlc"; then
+				BOOL_FALLBACK=true
+			elif ! maybe_apt_install "omxplayer"; then
+				BOOL_FALLBACK=true
 			fi
 			if [ $BOOL_FALLBACK = true ]; then
 				maybe_apt_install "ffplay" "ffmpeg"
@@ -58,11 +54,11 @@ fi
 
 if [ ! -e "$*" ]; then
 	echo "Error in $0 on line $LINENO. Exiting..."
-    exit 1
+	exit 1
 fi
 
 # check if able to run
-if is_which "cvlc" && is_opengl_legacy; then
+if is_which "cvlc"; then
 	STR_PROCESS="vlc"
 elif is_which "omxplayer"; then
 	STR_PROCESS="omxplayer.bin"
@@ -70,22 +66,20 @@ elif is_which "ffplay"; then
 	STR_PROCESS="ffplay"
 else
 	echo "Error in $0 on line $LINENO. Exiting..."
-    exit 1
+	exit 1
 fi
 
-BOOL_FALLBACK=false
+CMD_TEST=""
 if [ "$STR_PROCESS" = "vlc" ]; then
 	CMD_TEST="VLC_VERBOSE=0 cvlc $(quote_string_with_spaces "$*") vlc://quit --no-osd --fullscreen --align 0 --video-on-top --preferred-resolution -1 --play-and-exit"
-	eval "$CMD_TEST"
-	sleep 1
-	if ! is_process_running "vlc"; then
-		BOOL_FALLBACK=true
-	fi
 elif [ "$STR_PROCESS" = "omxplayer.bin" ]; then
 	CMD_TEST="omxplayer -b -o local --no-osd --timeout 5 $(quote_string_with_spaces "$*")"
-	eval "$CMD_TEST"
+fi
+BOOL_FALLBACK=false
+if [ ! "$CMD_TEST" = "" ]; then
+	eval "$CMD_TEST &"
 	sleep 1
-	if ! is_process_running "omxplayer.bin"; then
+	if ! is_process_running "$STR_PROCESS"; then
 		BOOL_FALLBACK=true
 	fi
 fi
